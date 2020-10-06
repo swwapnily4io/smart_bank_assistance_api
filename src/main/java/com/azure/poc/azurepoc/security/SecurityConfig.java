e3369bb92002bcfc39152abee0b70a222687ed4b
swwapnily4io <swwapnilyadav@gmail.com>
2020-10-06 17:01:31 +0530

    moving to main folder

---
+++ src/main/java/com/azure/poc/azurepoc/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+
+package com.azure.poc.azurepoc.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+public class SecurityConfig extends GlobalMethodSecurityConfiguration {
+
+  @Autowired
+  private OAuth2SecurityConfiguration securityConfig;
+
+  @Override
+  protected MethodSecurityExpressionHandler createExpressionHandler() {
+      return new OAuth2MethodSecurityExpressionHandler();
+  }
+}
